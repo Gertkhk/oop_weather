678a5e4794ca5e1e296c1152e3b4ae714fd283d3
Gertkhk <gert.sarv@khk.ee>
2022-11-09 14:40:55 +0200

    Get weather data from API

---
+++ classes/Weather.js
@@ -0,0 +1,12 @@
+class Weather{
+    constructor(city) {
+        this.city = city
+        this.key = "b6333456f56829f95df50b7a7cea7f1a"
+    }
+
+    async getWeather(){
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responceData = await responce.json()
+        return responceData
+    }
+}
