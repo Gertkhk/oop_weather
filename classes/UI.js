f080e093f314139056e7b6a5f3c06c654a2daec0
Gertkhk <gert.sarv@khk.ee>
2022-11-15 14:59:27 +0200

    Created style.css and updated HTML

--- classes/UI.js
+++ classes/UI.js
@@ -3,14 +3,29 @@ class UI {
         this.description = document.querySelector('#description')
         this.temp = document.querySelector('#temp')
         this.city = document.querySelector('#location')
+        this.windspeed = document.querySelector('#windspeed')
+        this.winddeg = document.querySelector('#winddeg')
+        this.Humidity = document.querySelector(`#Humidity`)
+        this.pressure = document.querySelector(`#pressure`)
+        this.visibility = document.querySelector(`#visibility`)
     }
 
     drawWeather(data) {
         let celcius = Math.round(parseFloat(data.main.temp)-273.15);
         let description = data.weather[0].description;
+        let winddeg = data.wind.deg;
+        let windspeed = data.wind.speed;
+        let humidity = data.main.humidity;
+        let pressure = data.main.pressure;
+        let visibility = data.visibility;
 
         this.description.innerHTML = description;
         this.temp.innerHTML = celcius + '&deg;';
         this.city.innerHTML = data.name;
+        this.winddeg.innerHTML = winddeg + '&deg;';
+        this.windspeed.innerHTML = windspeed + 'm/s';
+        this.Humidity.innerHTML = `Humidity ` + humidity + `%`;
+        this.pressure.innerHTML = `Pressure ` + pressure + ` psi`;
+        this.visibility.innerHTML = `Visibility ` + visibility + ` m`;
     }
 }
